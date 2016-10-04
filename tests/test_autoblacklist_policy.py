af53f4ab5ad53b8dd2798a9c62c2aa935d34c08c
Larry Tang <lryta0@gmail.com>
2016-10-04 21:37:23 +0800

    Refine logger

--- tests/test_autoblacklist_policy.py
+++ tests/test_autoblacklist_policy.py
@@ -4,7 +4,7 @@ import minpy
 import minpy.numpy as np
 from minpy.dispatch.policy import AutoBlacklistPolicy
 
-p = AutoBlacklistPolicy(gen_rule=True)
+p = AutoBlacklistPolicy(gen_rule=True, append_rule=True)
 
 minpy.set_global_policy(p)
 
@@ -13,5 +13,8 @@ print(a)
 b = np.array([50, 50])
 c = np.array([0, 0])
 np.add(a, b, c)
+print(c)
+np.add(a, b, out=c)
+np.add(a, b, out=c)
 print(c)
 
